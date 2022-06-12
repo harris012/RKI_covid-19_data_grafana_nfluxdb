45d6664fabc9351a2e58514ec00cd44c86317c41
harris012 <harissaeed@protonmail.com>
2022-06-12 19:58:56 +0200

    removed recovered cases

--- example.py
+++ example.py
@@ -5,7 +5,7 @@ from influxdb import InfluxDBClient
 client = InfluxDBClient(host="100.68.129.77", port=8086)
 client.switch_database("covid")
 
-URL = 'https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=1%3D1&outFields=GEN,BEZ,death_rate,cases,deaths,cases_per_100k,cases_per_population,BL,last_update,cases7_per_100k,recovered,cases7_bl_per_100k,cases7_bl,death7_bl,cases7_lk,death7_lk&outSR=4326&f=json'
+URL = 'https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=1%3D1&outFields=GEN,BEZ,death_rate,cases,deaths,cases_per_100k,cases_per_population,BL,last_update,cases7_per_100k,cases7_bl_per_100k,cases7_bl,death7_bl,cases7_lk,death7_lk&outSR=4326&f=json'
 result = requests.get(URL)
 
 output = json.loads(result.text)
@@ -28,7 +28,6 @@ for d in data:
 			"death7_bl":d['attributes']["death7_bl"],
 			"death7_lk":d['attributes']["death7_lk"],
 			"death_rate":float(d['attributes']["death_rate"]),
-			"recovered": d['attributes']["recovered"],
 			# TODO parse time to understandable by grafana
 			"last_update":d['attributes']["last_update"]
 		}}]
